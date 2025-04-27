068bb942c2de2b061da112fbd3ba5fb1414389c6
chandramadi <chandramadi6121@gmail.com>
2025-04-27 17:19:55 +0530

    language_detection

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 # Entry point
 from fastapi import FastAPI
 from app.api import predict, upload
+from fastapi.middleware.cors import CORSMiddleware # Add CORS middleware
 
 app = FastAPI(title="Indian Language Detection API")
+
+# Add CORS settings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Allow your React frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # allow all headers
+)
+
 app.include_router(predict.router)
 app.include_router(upload.router)
 
